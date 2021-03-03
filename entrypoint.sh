876ce6c755b8c5deb3dada9542f0b336bdc5b8db
Ahmet OZER <git@ahmetozer.org>
2021-03-03 04:00:11 +0300

    rtmp configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,44 +1,4 @@
-#!/bin/bash
-
-stream_servers=("twitch" "youtube" "facebook")
-if [ -z "$stream_key" ]; then
-    stream_key=$(tr </dev/urandom -dc _A-Z-a-z-0-9 | head -c${1:-32})
-    echo "Stream key is\t $stream_key"
-fi
-
-rtmp_port=${rtmp_port-1935}
-
-for stream_server in "${stream_servers[@]}"; do
-    if [ ! -z "${!stream_server}" ]; then
-        echo $stream_server stream key detected ${!stream_server}
-    fi
-done
-
-if ["$port" =~ $port_regex]; then
-    if (lsof -i :$port | grep TCP); then
-        echo "$cl_red Port already usage. Please select another port.$cl_nc"
-        exit 1
-    else
-        echo "Selected port $port/tcp"
-        break
-    fi
-else
-    echo "$cl_red Port is must be between 0-65535$cl_nc"
-    exit 1
-fi
-
-if [ ! -z "$clien_addr" ]; then
-    if [[ "$clien_addr" =~ $ip_regex ]] || [[ "$clien_addr" =~ $ip6_regex ]]; then
-        echo "Client addr is setted to \"$clien_addr\"\n other requests will be deny"
-        config_nginx_client_addr="
-        allow publish $clien_addr;
-        deny publish all;
-    "
-    else
-        echo "$cl_red Client addr must be IPv4 or IPv6 address \"$client_addr\"$cl_nc"
-        exit 1
-    fi
-fi
+#!/usr/bin/env bash
 
 ####
 #   Static variables
@@ -52,3 +12,181 @@ ip6_regex="^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|
 # Colors
 cl_red='\033[0;31m'
 cl_nc='\033[0m'
+cl_cy='\e[36m'
+cl_wh='\e[97m'
+
+stream_servers=("twitch" "youtube")
+if [ -z "$stream_key" ]; then
+    stream_key=$(tr </dev/urandom -dc A-Za-z0-9 | head -c${1:-32})
+    echo -e "\tStream key is ${cl_wh}${stream_key}${cl_nc}"
+
+fi
+
+rtmp_port=${rtmp_port-1935}
+stream_count=0
+for stream_server in "${stream_servers[@]}"; do
+    if [ ! -z "${!stream_server}" ]; then
+        echo -e "\t${cl_cy}${stream_server}${cl_nc} stream key detected ${!stream_server}"
+        stream_count=$((stream_count + 1))
+    fi
+done
+
+if [ $stream_count -lt 1 ]; then
+    echo -e "\t${cl_red}No stream key is defined.${cl_nc}"
+    exit 1
+fi
+
+if [[ "$rtmp_port" =~ $port_regex ]]; then
+    if (lsof -i :$rtmp_port | grep TCP); then
+        echo -e "${cl_red}Port already usage. Please select another port.${cl_nc}"
+        exit 1
+    else
+        echo -e "\tSelected port is $rtmp_port/tcp"
+    fi
+else
+    echo -e "${cl_red}Port is must be between 0-65535${cl_nc}"
+    exit 1
+fi
+
+if [ ! -z "$client_addr" ]; then
+    if [[ "$client_addr" =~ $ip_regex ]] || [[ "$client_addr" =~ $ip6_regex ]]; then
+        echo "Client addr is setted to \"$client_addr\"\n other requests will be deny"
+        config_nginx_client_addr="
+        allow publish $client_addr;
+        deny publish all;
+    "
+    else
+        echo -e "${cl_red}Client addr must be IPv4 or IPv6 address \"$client_addr\"${cl_nc}"
+        exit 1
+    fi
+fi
+
+### Nginx authorization server
+
+echo "
+ server {
+        listen 127.0.0.1:80 default_server;
+        #listen [::]:80 default_server;
+        # Everything is a 404
+        location / {
+                return 404;
+        }
+        # You may need this to prevent return 404 recursion.
+        location = /404.html {
+                internal;
+        }
+        location /auth {
+            if (\$arg_name = '$stream_key') {
+                return 200;
+            }
+            return 401;
+        }
+}
+" >/etc/nginx/conf.d/default.conf
+
+### Begin of the rtmp configuration
+echo "
+rtmp {
+        server {
+                listen $rtmp_port;
+                chunk_size 4096;
+                notify_method get;
+
+                application live {
+                             # Auth
+                             on_publish http://127.0.0.1/auth;
+                             live on;
+                             " >/etc/nginx/rtmp.conf
+
+# Stream save location check, if it is avaible, set to nginx
+
+# if [ -d "/record/" ]; then
+#     echo "Save location found, your streams will be saved."
+#     echo "
+#                              record all;
+#                              record_path /record/;
+#                              record_unique on;" >>/etc/nginx/rtmp.conf
+# fi
+
+for stream_server in "${stream_servers[@]}"; do
+    if [ ! -z "${!stream_server}" ]; then
+        echo "                             push rtmp://127.0.0.1:$rtmp_port/${stream_server};" >>/etc/nginx/rtmp.conf
+    fi
+done
+
+# End of the application live { section
+echo "                }" >>/etc/nginx/rtmp.conf
+
+# Twitch configuration
+if [ ! -z "$twitch" ]; then
+    echo "
+                      # Twitch Stream Application
+                      application twitch {
+                          live on;
+                          record off;
+                          allow publish 127.0.0.1;
+                          deny publish all;
+
+                          # Push URL with the Twitch stream key
+                          push rtmp://live-cdg.twitch.tv/app/$twitch;
+                      }" >>/etc/nginx/rtmp.conf
+fi
+
+# # Facebook configuration ## alpine nginx rtmp does not have a ssl client support, it can be done ssl client upgrade with nginx stream
+# if [ ! -z "$facebook" ]; then
+#     echo "
+
+#                       # Facebook Stream Application
+#                       application facebook {
+#                           live on;
+#                           record off;
+#                           allow publish 127.0.0.1;
+#                           deny publish all;
+
+#                           # Push URL with the Facebook stream key
+#                           push rtmps://live-api-s.facebook.com:443/rtmp/$facebook;
+
+#                       }
+# " >>/etc/nginx/rtmp.conf
+# fi
+
+# Facebook configuration
+if [ ! -z "$youtube" ]; then
+    echo "
+
+                      # YouTube Stream Application  
+                      application youtube {
+                          live on;
+                          record off;
+                          allow publish 127.0.0.1;
+                          deny publish all;
+
+                          # Push URL with the Facebook stream key
+                          push rtmp://a.rtmp.youtube.com/live2/$youtube;
+
+                      }
+" >>/etc/nginx/rtmp.conf
+fi
+
+### RTMP server and RTMP Class end
+echo "                  }
+}
+" >>/etc/nginx/rtmp.conf
+
+exit_trap() {
+    echo -e "\tStream server is closing"
+    PGID=$(ps -o pgid= $$ | tr -d \ )
+    kill -TERM -$PGID 2>/dev/null
+
+    echo "Server is closed"
+    exit 0
+}
+trap exit_trap INT EXIT
+#Start nginx
+nginx &
+NGINX_PID=$!
+wait $NGINX_PID
+if [ $? -eq 1 ]; then
+    echo -e "${cl_red}\tNginx shutdown is not done in gracefully${cl_nc}"
+    exit 1
+fi
